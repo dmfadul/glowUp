ad8d08aba18e68a15a8c1827d8e7e7d16cd799a5
dmfadul <dmf030@gmail.com>
2024-10-09 14:14:02 -0300

    added alembic and sqlachemy and created User object

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-
+db = SQLAlchemy()
 # login_manager = LoginManager()
 
 def create_app():
-    # config_filename = '../instance/config.py'
+    config_filename = '../instance/config.py'
 
     app = Flask(__name__)
+    app.config.from_pyfile(config_filename)
 
+    db.init_app(app)
+    
     # login_manager.init_app(app)
     # login_manager.login_view = 'login.login'
     # login_manager.login_message = 'Please log in to access this page.'
