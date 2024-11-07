e62341c0682bd1c19bc4cd6afc393cb1239fb740
dmfadul <dmf030@gmail.com>
2024-11-07 15:42:34 -0300

    added report.html and view-logs.html

--- app/routes/admin/admin.py
+++ app/routes/admin/admin.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 admin_bp = Blueprint('admin',
                     __name__,
                     template_folder='templates',
                     static_folder='static',
                     static_url_path='/admin/static')
+
+
+@admin_bp.route('/report')
+def index():
+    return render_template('report.html')
+
+
+@admin_bp.route('/view-logs')
+def view_logs():
+    return render_template('view-logs.html')
