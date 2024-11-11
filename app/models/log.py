256bb2a1e500d5c7b492a3561bd3f43f67ca3c7f
dmfadul <dmf030@gmail.com>
2024-11-11 13:48:48 -0300

    added add_log

--- app/models/log.py
+++ app/models/log.py
@@ -7,3 +7,9 @@ class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     info = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+    def add_log(self, info):
+        self.info = info
+        db.session.add(self)
+        db.session.commit()
